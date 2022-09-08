f8f080d374847aa98ceb089051ca134cd509791d
Endy4elect <endy4elect@yahoo.com>
2022-09-08 16:58:14 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * main - Entry point
+ * Description:print out "Programming is like building a multilingual puzzle"
+ * Return:Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
