9ea57b098773312846a599dc4e75b9a9fb17d285
Endy4elect <endy4elect@yahoo.com>
2022-09-08 15:55:26 -0700

    programming script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,11 +6,17 @@
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Description: print out "Programming is like
  *
- *     */
+ *     * building a multilingual puzzle
+ *
+ *      *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *        */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
-	return (0);
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
 }
