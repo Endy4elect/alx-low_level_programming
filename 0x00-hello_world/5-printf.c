852eccac57adc0d6435d9e78a771a3eb9d54954c
Endy4elect <endy4elect@yahoo.com>
2022-09-08 17:43:51 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description:print out "with proper grammar, but the outcome is a piece of art,\n"
+ * Description:print out "with proper grammar, but the outcome is a piece of art
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
