3bf9ef4b8e1a302c5792729f43ff94555e440087
Endy4elect <endy4elect@yahoo.com>
2022-09-08 17:25:39 -0700

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:print out "with proper grammar, but the outcome is a piece of art,\n"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
