4c66fc34e7e8302a4ec016f815ac2484bc97b72a
Endy4elect <endy4elect@yahoo.com>
2022-09-13 12:54:50 -0700

    all

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,18 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - alphabet 10 times
- * Return: Always 0
+ * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	char c, i;
 	
-	while (i < 10);
+	for (i = 0; i <= 9 i++);
 	{
-		char c;
-		
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar (c);
+			_putchar(c);
 		}
-		_putchar ('\n');
-		i++;
+		_putchar('\n');
 	}
 }
