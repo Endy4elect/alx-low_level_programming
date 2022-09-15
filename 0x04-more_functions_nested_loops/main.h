e8f43ea4cbca67c6c3994db1ef6b87eabc095090
Endy4elect <endy4elect@yahoo.com>
2022-09-15 09:39:16 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,9 +7,9 @@
  * int prototypes (int)
  */
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -18,5 +18,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
 
-#endif
+#endif /* MAIN_H */
