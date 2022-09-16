662cea5ee35970e6ba2ae0d86a6194625ca530ab
Endy4elect <endy4elect@yahoo.com>
2022-09-15 21:14:26 -0700

    main.h

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_number - Function that prints an integer.
+ * print_number - prints a number.
  * @n: int type number
  * Description: Can only use _putchar to print.
  */
