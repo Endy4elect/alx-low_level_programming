629200d614eddb1a653675feb78d6b6a74a0c480
Endy4elect <endy4elect@yahoo.com>
2022-09-16 02:22:36 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void prototypes (void)
-int prototypes (int)
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
