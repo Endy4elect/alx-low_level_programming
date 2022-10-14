e519612ca50278e2395ffe5f0727a0704e3a8db4
Endy4elect <endy4elect@yahoo.com>
2022-10-13 18:01:52 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - sums all arguments
@@ -8,7 +9,7 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i;
+	register unsigned int i;
 	int sum = 0;
 	va_list arguments;
 
